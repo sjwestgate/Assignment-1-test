492b629f78f99a38fded77c7738d1a50543c07a6
Caterina <caterina.doglioni@cern.ch>
2023-12-14 14:49:18 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 echo "Running tests..."
 echo
 
-output=$(./a.out)
-expected_output="Hello, World!" #this needs to be changed to the actual value of the energy we want
+output=$(./assignment-1)
+#expected_output="Hello, World!" #this needs to be changed to the actual value of the energy we want
 
 if [ $? -eq 0 ] ; then
   echo "Pass: Program exited zero"
@@ -11,14 +11,14 @@ else
   exit 1
 fi
 
-if [ "$output" == "$expected_output" ] ; then
-  echo "Pass: Output is correct"
-else
-  echo "Expected '$expected_output' but got: $output"
-  exit 1
-fi
+#if [ "$output" == "$expected_output" ] ; then
+#  echo "Pass: Output is correct"
+#else
+#  echo "Expected '$expected_output' but got: $output"
+#  exit 1
+#fi
 
 echo
-echo "All tests passed."
+echo "Your code compiles!"
 
 exit 0
