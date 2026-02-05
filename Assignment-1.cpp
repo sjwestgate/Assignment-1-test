a63d68000de5f5d9a115fc9c2a89629b03ded62b
Sarah Westgate <s.westgate@aol.co.uk>
2026-02-05 15:59:58 +0000

    Changing use std to std::

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -2,18 +2,17 @@
 // Program to calculate transition energy using simple Bohr formula
 
 #include <iostream>
-using namespace std;
 
 int main() {
   int Z;
-  cout << "Please enter an integer value for Z: "; // Type a number and press enter
-  while(!(cin >> Z) || Z <= 0) {
-    cin.clear(); // Reset input errors
-    cin.ignore(10000, '\n'); // Remove bad input
-    cout << "That's not a positive integer! Please enter a positive integer value for Z: ";
+  std::cout << "Please enter an integer value for Z: "; // Type a number and press enter
+  while(!(std::cin >> Z) || Z <= 0) {
+    std::cin.clear(); // Reset input errors
+    std::cin.ignore(10000, '\n'); // Remove bad input
+    std::cout << "That's not a positive integer! Please enter a positive integer value for Z: ";
   }
 
-  cout << "Your number is: " << Z << endl;
+  std::cout << "Your number is: " << Z << std::endl;
   return 0;
 }
 
