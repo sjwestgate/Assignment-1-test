e05662936e74f46443b2ee9032621e087d16085c
Sarah Westgate <s.westgate@aol.co.uk>
2026-02-05 15:55:32 +0000

    Validating input for Z

--- Assignment-1.cpp
+++ Assignment-1.cpp
@@ -7,8 +7,13 @@ using namespace std;
 int main() {
   int Z;
   cout << "Please enter an integer value for Z: "; // Type a number and press enter
-  cin >> Z; // Get user input from the keyboard
-  cout << "Your number is: " << Z;
+  while(!(cin >> Z) || Z <= 0) {
+    cin.clear(); // Reset input errors
+    cin.ignore(10000, '\n'); // Remove bad input
+    cout << "That's not a positive integer! Please enter a positive integer value for Z: ";
+  }
+
+  cout << "Your number is: " << Z << endl;
   return 0;
 }
 
